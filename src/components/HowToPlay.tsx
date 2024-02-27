d32bb8c0e0cef791dd3b6606e2ce74422ab23454
Omari <marzipant.e@gmail.com>
2024-02-27 16:11:41 +0000

    Improve how to page

--- src/components/HowToPlay.tsx
+++ src/components/HowToPlay.tsx
@@ -5,7 +5,14 @@ import { DialogControlProps, ModalDialog } from './ModalDialog';
 import { useEffect, useState } from 'react';
 import { MAX_LIVES } from '@/features/game/consts';
 import { useGameStore } from '@/features/game/stores';
-
+import { WithChildrenProps } from '@/types';
+function Bold({ children }: WithChildrenProps) {
+    return (
+        <span className="rounded-full bg-primary-400 px-2 py-1 font-semibold text-light">
+            {children}
+        </span>
+    );
+}
 export function HowToPlay() {
     const [dialogOpen, setDialogOpen] = useState(false);
 
@@ -23,15 +30,27 @@ export function HowToPlay() {
                     setDialogOpen(value);
                 }}>
                 Link each word to the next e.g.:
-                <h4>Harmony, Discord, Slack</h4>
+                <h4 className="mx-auto flex flex-row flex-wrap gap-3">
+                    <Bold>Harmony</Bold>
+                    <Bold>Discord</Bold>
+                    <Bold>Slack</Bold>
+                </h4>
                 <ul>
-                    <li>Harmony -&gt; Discord: Antonyms</li>
-                    <li>Discord -&gt; Slack: Communcation Platforms</li>
+                    <li>
+                        <Bold>Harmony</Bold> → <Bold>Discord</Bold> : Antonyms
+                    </li>
+                    <li>
+                        <Bold>Discord</Bold> → <Bold>Slack</Bold> : Communcation
+                        Platforms
+                    </li>
                 </ul>
                 Each puzzle has exactly one solution, so watch out for words
                 that look like they may connect to multiple words. You have{' '}
-                {MAX_LIVES} lives. Good luck! If you liked this game, say{' '}
-                <a href="https://twitter.com/marile0n">hi :)</a>
+                {MAX_LIVES} lives. Good luck!{' '}
+                <p>
+                    If you liked this game, or if you have any feedback, say{' '}
+                    <a href="https://twitter.com/marile0n">hi :)</a>
+                </p>
             </ModalDialog>
         </>
     );
